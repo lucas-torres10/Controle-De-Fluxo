3012e627948fc37db47a972cbe1386a773e47ad5
Lucas Torres <lucas.ctorres213@gmail.com>
2025-04-09 09:34:33 -0300

    Desafio Completo

---
+++ src/main/java/org/example/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
+
